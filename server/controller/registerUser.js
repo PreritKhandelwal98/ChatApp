b063a0d599ee60fe646a2e712188f3706d954d20
Prerit Khandelwal <prerit.web@gmail.com>
2024-05-21 18:39:38 +0530

    valid email enter implimented

--- server/controller/registerUser.js
+++ server/controller/registerUser.js
@@ -1,10 +1,12 @@
 const UserModel = require("../models/UserModel")
 const bcrypt = require('bcrypt')
+const axios = require('axios');
 async function registerUser(req, res) {
     try {
         const { name, email, password, profile_pic } = req.body
 
         const checkEmail = await UserModel.findOne({ email });
+
 
         //checking user presence 
         if (checkEmail) {
@@ -13,6 +15,39 @@ async function registerUser(req, res) {
                 error: true
             })
         }
+
+        const hunterApiKey = process.env.HUNTER_API_KEY;
+
+        const hunterUrl = `https://api.hunter.io/v2/email-verifier?email=${email}&api_key=${hunterApiKey}`;
+
+        let emailVerificationResponse;
+        try {
+            emailVerificationResponse = await axios.get(hunterUrl);
+        } catch (err) {
+            if (err.response && err.response.status === 400) {
+                console.error("Invalid email address:", err.response.data);
+                return res.status(400).json({
+                    message: "Invalid email address",
+                    error: true
+                });
+            } else {
+                console.error("Email verification error:", err.response ? err.response.data : err.message);
+                return res.status(500).json({
+                    message: "Email verification failed",
+                    error: true
+                });
+            }
+        }
+
+        const emailStatus = emailVerificationResponse.data.data.status;
+        // Check the email verification status
+        if (emailStatus !== 'valid') {
+            return res.status(400).json({
+                message: "Invalid email address",
+                error: true
+            });
+        }
+
 
         //password into hashed
         const salt = await bcrypt.genSalt(10);
@@ -35,7 +70,7 @@ async function registerUser(req, res) {
             success: true
         })
     } catch (err) {
-        return res.status(500).josn({
+        return res.status(500).json({
             message: err.message || err,
             err: true
         })
